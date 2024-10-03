dcaa7df874bd867bb2fc36cec87173b1e97619dd
GwonDooHyeon <secure3141@gmail.com>
2024-10-04 07:31:16 +0900

    project init

---
+++ src/main/java/sample/cafekioskreview/CafekioskReviewApplication.java
@@ -0,0 +1,13 @@
+package sample.cafekioskreview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafekioskReviewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CafekioskReviewApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sample/cafekioskreview/CafekioskReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package sample.cafekioskreview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafekioskReviewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
