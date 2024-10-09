17255c68275a10853df9f1973a7d15c4d7d5b8c5
GwonDooHyeon <secure3141@gmail.com>
2024-10-09 23:24:28 +0900

    영속성 테스트2

--- src/main/java/sample/cafekioskreview/spring/domain/product/Product.java
+++ src/main/java/sample/cafekioskreview/spring/domain/product/Product.java
@@ -2,6 +2,7 @@ package sample.cafekioskreview.spring.domain.product;
 
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import sample.cafekioskreview.spring.domain.BaseEntity;
@@ -10,22 +11,31 @@ import sample.cafekioskreview.spring.domain.BaseEntity;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 public class Product extends BaseEntity {
-
+    
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
+    
     private String productNumber;
-
+    
     @Enumerated(EnumType.STRING)
     private ProductType type;
-
+    
     @Enumerated(EnumType.STRING)
     private ProductSellingStatus sellingStatus;
-
+    
     private String name;
-
+    
     private int price;
-
+    
+    @Builder
+    private Product(String productNumber, ProductType type, ProductSellingStatus sellingStatus, String name, int price) {
+        this.productNumber = productNumber;
+        this.type = type;
+        this.sellingStatus = sellingStatus;
+        this.name = name;
+        this.price = price;
+    }
+    
 }
 
